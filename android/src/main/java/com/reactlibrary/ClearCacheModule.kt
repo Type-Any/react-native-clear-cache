aaa885b3aeb8cfd7342963a8b756bd99c33be221
sanghyeok hong <ghdtkdgur123@gmail.com>
2020-09-14 23:23:50 +0900

    Update ClearCacheModule.kt
    revert package, constructor

--- android/src/main/java/com/reactlibrary/ClearCacheModule.kt
+++ android/src/main/java/com/reactlibrary/ClearCacheModule.kt
@@ -1,8 +1,12 @@
+package com.reactlibrary
+
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 
 class ClearCache(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+    private val reactContext: ReactApplicationContext
+
     override fun getName(): String {
         return "ClearCache"
     }
@@ -15,5 +19,9 @@ class ClearCache(reactContext: ReactApplicationContext) : ReactContextBaseJavaMo
     @ReactMethod
     public fun clearAppCache() {
         // TODO What you want to do
+    }
+
+    init {
+        this.reactContext = reactContext
     }
 }
