9bf319e46d62dcdf3e8517183c861c69084dfc08
sanghyeok hong <ghdtkdgur123@gmail.com>
2020-09-14 23:25:31 +0900

    Update ClearCachePackage.kt
    revert package
    replace Collecton.emptyList to mutableListOf

--- android/src/main/java/com/reactlibrary/ClearCachePackage.kt
+++ android/src/main/java/com/reactlibrary/ClearCachePackage.kt
@@ -1,17 +1,19 @@
+package com.reactlibrary
+
 import android.view.View
+
 import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
-import java.util.*
 
 class ClearCachePackage : ReactPackage {
-
-    override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<ClearCachePackage> {
-        return mutableListOf(ClearCachePackage(reactContext))
+    override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
+        return mutableListOf(ClearCache(reactContext))
     }
 
-    override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> {
-        return Collections.emptyList()
+    override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<out View, out ReactShadowNode<*>>> {
+        return mutableListOf()
     }
 }
