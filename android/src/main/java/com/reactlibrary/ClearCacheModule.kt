41b755286003df0bb0f819a480d464e39017495d
Seongjoo Jin <evanjin8@gmail.com>
2020-09-14 22:39:40 +0900

    Modify Type Definition&Native Module Preferences

--- android/src/main/java/com/reactlibrary/ClearCacheModule.kt
+++ android/src/main/java/com/reactlibrary/ClearCacheModule.kt
@@ -1,24 +1,19 @@
-package com.reactlibrary
-
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Callback
 
-class ClearCacheModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
-    private val reactContext: ReactApplicationContext
-
-    @get:Override
-    val name: String
-        get() = "ClearCache"
-
-    @ReactMethod
-    fun sampleMethod(stringArgument: String, numberArgument: Int, callback: Callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: $numberArgument stringArgument: $stringArgument")
+class ClearCache(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+    override fun getName(): String {
+        return "ClearCache"
     }
 
-    init {
-        this.reactContext = reactContext
+    @ReactMethod
+    public fun getAppCacheSize() {
+        // TODO What you want to do
+    }
+
+    @ReactMethod
+    public fun clearAppCache() {
+        // TODO What you want to do
     }
 }
--- android/src/main/java/com/reactlibrary/ClearCachePackage.kt
+++ android/src/main/java/com/reactlibrary/ClearCachePackage.kt
@@ -1,22 +1,17 @@
-package com.reactlibrary
-
-import java.util.Arrays
-import java.util.Collections
-import java.util.List
+import android.view.View
 import com.facebook.react.ReactPackage
-import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
-import com.facebook.react.bridge.JavaScriptModule
+import java.util.*
 
 class ClearCachePackage : ReactPackage {
-    @Override
-    fun createNativeModules(reactContext: ReactApplicationContext?): List<NativeModule> {
-        return Arrays.< NativeModule > asList < NativeModule ? > ClearCacheModule(reactContext)
+
+    override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<ClearCachePackage> {
+        return mutableListOf(ClearCachePackage(reactContext))
     }
 
-    @Override
-    fun createViewManagers(reactContext: ReactApplicationContext?): List<ViewManager> {
+    override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> {
         return Collections.emptyList()
     }
 }
