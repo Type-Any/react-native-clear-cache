62f0d375e4290d0d76cbf167dcec49a9d382ded3
Seongjoo Jin <evanjin8@gmail.com>
2020-09-27 21:24:58 +0900

    ios setting

--- ios/ClearCacheBridge.m
+++ ios/ClearCacheBridge.m
@@ -1,6 +1,8 @@
+#import "ClearCache.h"
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(ClearCache, NSObject)
     RCT_EXTERN_METHOD(getAppCacheSize)
     RCT_EXTERN_METHOD(clearAppCache)
+
 @end
