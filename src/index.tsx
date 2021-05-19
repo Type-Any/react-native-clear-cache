cb45ea8d277e1a9a534b6f74e8db82fc55115f48
sang hyeok hong <sanghyeokhong@sangui-MacBookAir.local>
2021-05-19 14:31:35 +0900

    test: add test code for foramtFileSize func

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,7 @@ export function foramtFileSize(size: number, decimal: number = 4): string {
 
   const arr = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   const i = Math.floor(Math.log(size) / Math.log(1024));
+  const d = decimal < 0 ? 0 : decimal > 4 ? 4 : decimal;
 
-  return parseFloat((size / Math.pow(1024, i)).toFixed(decimal)) + ' ' + arr[i];
+  return parseFloat((size / Math.pow(1024, i)).toFixed(d)) + ' ' + arr[i];
 }
